2b6752ebc69dc0e92ffd013d0e587ee476216b5c
Neo Koda <mneocicerok@gmail.com>
2025-11-26 15:18:14 +0700

    fix : integration with frontend

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from src.pipeline.langgraph_pipeline import flashcard_graph, PipelineState
 
 app = FastAPI(
     title="Automatic Flashcard Generator API",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class GenerateRequest(BaseModel):
@@ -18,9 +27,33 @@ class GenerateResponse(BaseModel):
     difficulty: list
     translations: list
 
+# @app.post("/mock/generate", response_model=GenerateResponse)
+# def mock_generate_flashcard(req: GenerateRequest):
+#     mock_sentences = [
+#         {"sentence": "I really appreciate your help with this project.", "similarity": 0.95},
+#         {"sentence": "Could you please submit the report by tomorrow?", "similarity": 0.88},
+#         {"sentence": "I promise to call you as soon as I arrive.", "similarity": 0.82}
+#     ]
+
+#     mock_intents = ["Inform", "Directive", "Commissive"]
+
+#     mock_difficulty = ["B1", "B2", "A2"]
+
+#     mock_translations = [
+#         "Saya sangat menghargai bantuan Anda dalam proyek ini.",
+#         "Bisakah Anda mengirimkan laporannya besok?",
+#         "Saya berjanji akan menelepon Anda segera setelah saya tiba."
+#     ]
+
+#     return GenerateResponse(
+#         top_sentences=mock_sentences,
+#         intents=mock_intents,
+#         difficulty=mock_difficulty,
+#         translations=mock_translations
+#     )
+
 @app.post("/generate", response_model=GenerateResponse)
 def generate_flashcard(req: GenerateRequest):
-
     init_state = PipelineState(
         topic=req.topic,
         text=req.text,
